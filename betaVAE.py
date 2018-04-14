9c95caa46b95560aabd751008725458b561bd333
stoplime <slanimero@gmail.com>
2018-04-13 20:40:41 -0400

    imrpoved testing on bVAE

--- betaVAE.py
+++ betaVAE.py
@@ -32,17 +32,14 @@ import cv2
 class Agent(object):
     def __init__(self, action_space):
         self.state_shape = (210, 160, 3)
-        self.model_shape = (256, 256, 3)
-        self.batchSize = 10
+        self.model_shape = (128, 128, 3)
+        self.batchSize = 64
         self.latentSize = 100
 
         self.action_space = action_space
         self.memory = deque(maxlen=2000)
 
-        self.frameSkip = 10
-        self.frameCount = 0
-
-        encoder = Darknet19Encoder(self.model_shape, self.batchSize, self.latentSize, 'bvae')
+        encoder = Darknet19Encoder(self.model_shape, self.batchSize, self.latentSize, None)
         decoder = Darknet19Decoder(self.model_shape, self.batchSize, self.latentSize)
         self.vae = AutoEncoder(encoder, decoder)
         self.vae.ae.compile(optimizer='adam', loss='mean_absolute_error')
@@ -50,12 +47,12 @@ class Agent(object):
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-    def processImage(self, state, batchsize=1):
+    def processImage(self, state):
         batch = np.float32(state)/255 - 0.5
         return batch
         # return cv2.resize(batch, self.model_shape[:2], interpolation=cv2.INTER_LINEAR)
 
-    def unprocessImage(self, state, batchsize=1):
+    def unprocessImage(self, state):
         state[state > 0.5] = 0.5 # clean it up a bit
         state[state < -0.5] = -0.5
         _state = np.uint8((state+0.5)*255)
@@ -71,7 +68,7 @@ class Agent(object):
         # return np.argmax(act_values[0])
 
     def train(self, batch_images):
-        batch_images = self.processImage(batch_images, self.batchSize)
+        batch_images = self.processImage(batch_images)
 
         self.vae.ae.fit(batch_images, batch_images,
                     epochs=1,
@@ -82,8 +79,8 @@ class Agent(object):
 
     def get_predict(self, state):
         tileImage = self.processImage(state)
-        pred = self.vae.ae.predict(np.squeeze(np.array([tileImage]*self.batchSize)))
-        return self.unprocessImage(pred, self.batchSize)[0]
+        pred = self.vae.ae.predict(np.squeeze(np.array([tileImage]*self.batchSize)), batch_size=self.batchSize)
+        return self.unprocessImage(pred)[0]
 
     def replay(self):
         # print("learning")
@@ -97,19 +94,19 @@ class Agent(object):
     
     def load(self, name):
         # TODO: Implement this
-        # self.model.load_weights(name)
+        self.vae.ae.load_weights(name)
         pass
 
     def save(self, name):
         # TODO: Implement this
-        # self.model.save_weights(name)
+        self.vae.ae.save_weights(name)
         pass
 
 def convertImage(image):
     uint8Image = np.array(image, dtype=np.uint8)
     pilImage = Image.fromarray(uint8Image)
-    resizedImage = pilImage.resize((256, 256))
-    numpyImage = np.array(resizedImage, dtype=np.float16)
+    resizedImage = pilImage.resize((128, 128), resample=Image.LANCZOS)
+    numpyImage = np.array(resizedImage, dtype=np.uint8)
     return numpyImage
 
 if __name__ == '__main__':
@@ -117,6 +114,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('env_id', nargs='?', default='MontezumaRevenge-v0', help='Select the environment to run')
     args = parser.parse_args()
+
+    folder = "./save/images_3"
+    # model - number - modelType - betaValue - inputShape
+    saveFile = "model-3-bVAE-0-128px"
+
+    load = False
+    save = True
 
     # You can set the level to logger.DEBUG or logger.WARN if you
     # want to change the amount of output.
@@ -134,12 +138,14 @@ if __name__ == '__main__':
     # env = wrappers.Monitor(env, directory=outdir, force=True)
     env.seed(0)
     agent = Agent(env.action_space)
-    agent.load("./save/montazuma-dqn.h5")
+    if load:
+        agent.load(sys.path.join(folder, saveFile+".h5"))
 
     # writer = tf.summary.FileWriter('log')
     # writer.add_graph(tf.get_default_graph())
 
-    episode_count = 100
+    episode_count = 1000
+    maxTime = 1000
     reward = 0
     done = False
 
@@ -148,6 +154,7 @@ if __name__ == '__main__':
         ob = convertImage(ob)
         ob = np.expand_dims(ob, axis=0)
         time = 0
+        sampleTime = random.randint(0, maxTime)
         while True:
             time += 1
             action = agent.act(ob, reward, done)
@@ -157,26 +164,30 @@ if __name__ == '__main__':
 
             agent.remember(ob, action, reward, new_ob, done)
 
-            if len(agent.memory) > agent.batchSize and time % 10 == 0:
+            if len(agent.memory) > agent.batchSize and time % agent.batchSize == 0:
                 print("episode: {}/{}, time: {}"
                       .format(i+1, episode_count, time))
                 agent.replay()
-            if time == 500:
+            
+            if done or time == sampleTime:
                 # pred = Image.fromarray(agent.get_predict(ob))
                 # pred.show()
-                cvPred = cv2.cvtColor(agent.get_predict(ob), cv2.COLOR_RGB2BGR)
-                cv2.imwrite("save/images/sample_{}.png".format(str(i).zfill(3)), cvPred)
+                pred = agent.get_predict(ob)
+                visualize = np.concatenate((np.squeeze(ob), pred), axis=1)
+                cvPred = cv2.cvtColor(visualize, cv2.COLOR_RGB2BGR)
+                cv2.imwrite(sys.path.join(folder, "sample_{}.png".format(str(i).zfill(3))), cvPred)
                 cv2.imshow("image", cvPred)
                 cv2.waitKey(1)
 
             ob = new_ob
             env.render()
-            if done or time >= 1000:
+            if done or time >= maxTime:
                 print("episode: {}/{}, time: {}"
                       .format(i+1, episode_count, time))
                 if len(agent.memory) > agent.batchSize:
                     agent.replay()
-                # agent.save("./save/montazuma-dqn.h5")
+                if save:
+                    agent.save(sys.path.join(folder, saveFile+".h5"))
                 break
             # Note there's no env.render() here. But the environment still can open window and
             # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
